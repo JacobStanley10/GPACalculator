3d6a6fcb58faa5b2744ca2c0b7f45665ad68a67e
jakestan <jakestan@umich.edu>
2020-08-26 18:45:18 -0400

    MVP commit

--- script.js
+++ script.js
@@ -2,18 +2,20 @@ var first = document.getElementById('first');
 var second = document.getElementById('second');
 var third = document.getElementById('third')
 var fourth = document.getElementById('fourth')
+
 var result = document.getElementById('result');
 
 first.addEventListener("input", sum);
 second.addEventListener("input", sum);
+third.addEventListener("input", sum);
+fourth.addEventListener("input", sum);
 
 function sum() {
-  
- var one = parseFloat(first.value) || 0;
+var one = parseFloat(first.value) || 0;
 var two = parseFloat(second.value) || 0;
+var three = parseFloat(third.value) || 0;
+var four = parseFloat(fourth.value) || 0;
   
-var add = one+two;
-
-result.innerHTML = "Your Sum is : " + add;
-
+var GPA = (one + two + three + four)/4;
+result.innerHTML = "Your GPA is : " + GPA;
 }
