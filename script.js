bf78cf82a5253fbda9ebf0a964331f6f8fe7871d
jakestan <jakestan@umich.edu>
2020-09-01 23:11:22 -0400

    Added functionalities that include previous semesters GPA and credit as well as adding input fields for each classes credit value

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 var first = document.getElementById('first');
 var second = document.getElementById('second');
-var third = document.getElementById('third')
-var fourth = document.getElementById('fourth')
+var third = document.getElementById('third');
+var fourth = document.getElementById('fourth');
+var prevGPAButton = document.getElementById('prevGPAButton');
+var hiddenInputOfPrevGPA = document.getElementById('prevGPA');
+var hiddenInputOfPrevCredits = document.getElementById('prevCredits');
+var firstCredit = document.getElementById('firstCredit');
+var secondCredit = document.getElementById('secondCredit');
+var thirdCredit = document.getElementById('thirdCredit');
+var fourthCredit = document.getElementById('fourthCredit');
 
 var arrayOfAllInputs = [first, second, third, fourth];
 var arrayOfGrades = [];
+var arrayOfCreditsInitial = [firstCredit, secondCredit, thirdCredit, fourthCredit];
+var arrayOfCreditsFinal = [];
+var testCount = document.getElementById('testCount');
 
 
 var result = document.getElementById('result');
 
-
+hiddenInputOfPrevGPA.addEventListener("input", sum);
+hiddenInputOfPrevCredits.addEventListener("input",sum);
 first.addEventListener("input", sum);
+firstCredit.addEventListener("input", sum);
 second.addEventListener("input", sum);
+secondCredit.addEventListener("input", sum);
 third.addEventListener("input", sum);
+thirdCredit.addEventListener("input", sum);
 fourth.addEventListener("input", sum);
+fourthCredit.addEventListener("input", sum);
 
-
+function add(){
+  hiddenInputOfPrevGPA.type="text";
+  hiddenInputOfPrevCredits.type="text";
+  prevGPAButton.type="hidden";
+}
 function upperCaseAndGradePoint(a){
 var gradeBook = {"A+" : 4.0, "A" : 4.0, "A-" : 3.7, "B+" : 3.3, "B" : 3.0, "B-" : 2.7, "C+" : 2.3, "C" : 2.0, "C-" : 1.7, "D+" : 1.3, "D" : 1.0, "D-" : 0.7, "E" : 0};
 var grade = a.toUpperCase();
@@ -25,19 +44,26 @@ return gradePoint;
 
 function sum() {
 for(i = 0; i < arrayOfAllInputs.length; i++){
-  if(arrayOfAllInputs[i].value != 0){
+  if((arrayOfAllInputs[i].value != 0) && (arrayOfCreditsInitial[i] != 0)){
     arrayOfGrades.push(arrayOfAllInputs[i]);
+    arrayOfCreditsFinal.push(arrayOfCreditsInitial[i]);
   }
 }
 var totalInputs = arrayOfGrades.length;
-var totalGradePoint = 0;
+var totalGradePoint = hiddenInputOfPrevGPA.value * hiddenInputOfPrevCredits.value;
+var totalCredits = hiddenInputOfPrevCredits.value;
+//upperCaseAndGradePoint(arrayOfGrades[0].value) * arrayOfCreditsFinal[0].value;
 for(i = 0; i < totalInputs; i++){
-totalGradePoint+=upperCaseAndGradePoint(arrayOfGrades[i].value);
-}
-var GPA = totalGradePoint/totalInputs;
+totalGradePoint+=(upperCaseAndGradePoint(arrayOfGrades[i].value)) * arrayOfCreditsFinal[i].value;
+// testCount.innerHTML = totalCredits + arrayOfCreditsFinal[i].value;
+totalCredits+=arrayOfCreditsFinal[i].value;
 
+  //"Total Credits: " + totalCredits + " Total Grade Point: " + totalGradePoint;
+} 
+var GPA = totalGradePoint/totalCredits;
+
+result.innerHTML = "Your GPA is : " + GPA;
 if(isNaN(GPA)){
   test.innerHTML = "Please enter valid letter grades as inputs";
 }
-result.innerHTML = "Your GPA is : " + GPA;
 }
